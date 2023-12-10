566a9d00081ce0ee71a448c1dbd51ad2ec0d7e9d
Falmata Bongo <bongofalmata22@gmail.com>
2023-12-10 15:06:15 +0000

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { CategoryFormsComponent } from './category-forms/category-forms.component';
+import { ProductFormsComponent } from './product-forms/product-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CategoryComponent,
+    CategoryFormsComponent,
+    ProductFormsComponent
   ],
   imports: [
     BrowserModule,
